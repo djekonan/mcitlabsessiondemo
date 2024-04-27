ecff7144c70521777b6d660d6676aec90e7f1a65
djekonan <78828468+djekonan@users.noreply.github.com>
2024-04-27 06:48:56 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,11 +1,25 @@
 locals{
   servicename="abracadabra"
   forum="abracadabramcit"
+  firsname="konan"
+  lastname="hyacinthe"
+  city="victoriaville"
   lengthsa=length(local.servicename)
   lengthforum=length(local.forum)
 }
 output "print"{
     value = local.servicename
+}
+
+}
+output "printfirsname"{
+    value = local.firsname
+}
+output "printlastname{
+    value = local.lastname
+}
+output "printcity{
+    value = local.city
 }
 output "secondprint"{
     value = local.forum
