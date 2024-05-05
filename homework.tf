4f6d85757afdaa9063709665d791fdc9b92dbf20
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-05 15:14:26 -0400

    Update homework.tf

--- homework.tf
+++ homework.tf
@@ -1,4 +1,18 @@
 // ******************** HOMEWORK ************************
+
+
+
+1
+variable "nested_map" {
+  type = map(map(string))
+  default = {
+    group1 = {"name" = "Alice", "age" = "25"}
+    group2 = {"name" = "Bob", "age" = "30"}
+    group3 = {"name" = "Charlie", "age" = "35"}
+  }
+}
+
+2
 variable "total_output" {
   type    = list(string)
   default = ["150", "150", "150"]
