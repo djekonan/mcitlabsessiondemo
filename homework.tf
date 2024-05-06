a7c61cdbbdd541f4137377278e1ff675e9b4164f
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-06 19:02:01 -0400

    Update homework.tf

--- homework.tf
+++ homework.tf
@@ -7,45 +7,10 @@ locals {
 }
 
 
-
-variable "nested_map" {
-  type = map(map(string))
-  default = {
-    group1 = {"name" = "Alice", "age" = "25"}
-    group2 = {"name" = "Bob", "age" = "30"}
-    group3 = {"name" = "Charlie", "age" = "35"}
-  }
+output "cloud_check" {
+  value = contains(local.clouds, "azure") ? "Azure is part of the cloud list" : "Azure is not part of the cloud list"
 }
 
-
-variable "total_output" {
-  type    = list(string)
-  default = ["150", "150", "150"]
-}
-
-variable "winterlistOfSports" {
-  type    = list(string)
-  default = ["icehockey", "snowboarding", "iceskating"]
-}
-
-locals {
-  extensive = length(var.winterlistOfSports) > 3
-}
-
-
-output "updated_nested_map" {
-  value = var.nested_map
-}
-output "incremented_output" {
-  value = local.incremented_output
-}
-
-locals {
-  incremented_output = [for num in var.total_output : tostring(tonumber(num) + 10)]
-}
-output "Winter_sports_list_status" {
-  value = local.extensive ? "Winter sports list is extensive" : "Winter sports list is limited"
-}
 
 
 output "cloud_ownership" {
