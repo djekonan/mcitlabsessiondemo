f864f407392191877a9be0a4dac9a20c15a117f5
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-06 19:16:26 -0400

    Update homework.tf

--- homework.tf
+++ homework.tf
@@ -5,10 +5,22 @@ locals {
   cloud_owners = ["Microsoft", "Amazon", "Google"]
   cloud_owner_map = zipmap(local.clouds, local.cloud_owners)
 }
+locals {
+  nested_list = [[1,2,3],[1,2],[1,2,3,4],[1,2,3,4,5]]
+  flattened_list = flatten(local.nested_list)
+  sum_of_elements = sum(local.flattened_list)
+}
 
 
 output "cloud_check" {
   value = contains(local.clouds, "azure") ? "Azure is part of the cloud list" : "Azure is not part of the cloud list"
+}
+output "flattened_list" {
+  value = local.flattened_list
+}
+
+output "sum_of_elements" {
+  value = local.sum_of_elements
 }
 
 
