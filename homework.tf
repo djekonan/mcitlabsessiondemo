08a7e5e9c51b0ebeee538592fb7816fcc565b238
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-06 19:28:18 -0400

    Update homework.tf

--- homework.tf
+++ homework.tf
@@ -5,10 +5,11 @@ locals {
   cloud_owners = ["Microsoft", "Amazon", "Google"]
   cloud_owner_map = zipmap(local.clouds, local.cloud_owners)
 }
-locals {
-  nested_list = [[1,2,3],[1,2],[1,2,3,4],[1,2,3,4,5]]
-  flattened_list = flatten(local.nested_list)
-  sum_of_elements = sum(local.flattened_list)
+
+Locals {
+  konan = [[1,2,3],[1,2],[1,2,3,4],[1,2,3,4,5]]
+  flatkonan = flatten(local.konan)
+  sumkonfat = sum(local.flatkonan)
 }
 
 
