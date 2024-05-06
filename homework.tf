8a55abc561ddafde09504a09777263481e8c188d
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-06 19:12:25 -0400

    Update homework.tf

--- homework.tf
+++ homework.tf
@@ -6,9 +6,6 @@ locals {
   cloud_owner_map = zipmap(local.clouds, local.cloud_owners)
 }
 
-output "cloud_ownership" {
-  value = local.cloud_owner_map
-}
 
 output "cloud_check" {
   value = contains(local.clouds, "azure") ? "Azure is part of the cloud list" : "Azure is not part of the cloud list"
