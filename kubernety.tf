8fa25efb41113d8ded941498aa7c1dac367bb1a4
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-08 18:56:03 -0400

    Update kubernety.tf

--- kubernety.tf
+++ kubernety.tf
@@ -2,7 +2,7 @@
 locals{ 
   cluster_names=["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
 }
-resource "azurerm_resource_group" "azureresourcegroup" {
+resource "azurerm_resource_group" "azurekongroup" {
   name     = "MCIT_konan_group"
   location = "Canada Central"
 }
