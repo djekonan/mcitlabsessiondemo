122adb5399ef0d786fc9b7a1219cbe48dada6028
djekonan <78828468+djekonan@users.noreply.github.com>
2024-05-08 20:07:51 -0400

    Update kubernety.tf

--- kubernety.tf
+++ kubernety.tf
@@ -1,8 +1,41 @@
-#This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
-locals{ 
-  cluster_names=["Abidjan","Man",Bassam","Daloa","Sanpedro"]
+provider "azurerm" {
+  features {}
 }
-resource "azurerm_resource_group" "Abidjan" {
-  name     = "konan_MCIT_resource_group"
-  location = "Canada Central"
+
+variable "clusterlist" {
+  type    = list(string)
+  default = ["montreal", "toronto", "vancouver", "alberta"]
+}
+
+variable "environment" {
+  type    = string
+  default = "production"
+}
+
+resource "azurerm_resource_group" "konangp" {
+  name     = "datacentes"
+  location = "USA"
+}
+
+resource "azurerm_kubernetes_cluster" "konangp" {
+  for_each            = toset(var.clusterlist) // Use toset to ensure unique values
+  name                = "${each.value}-${var.environment}-cluster"
+  location            = azurerm_resource_group.konangp.location
+  resource_group_name = azurerm_resource_group.konangp.name
+  dns_prefix          = "${each.value}-${var.environment}-cluster"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
 }
